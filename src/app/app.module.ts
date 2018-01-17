1a600008409e1a4ba8bd0db42205c97ffb5eb12c
TopherMcK <123topher456@gmail.com>
2018-01-17 14:51:35 -0600

    importin http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FindCollabComponent } from './find-collab/find-collab.component';
+import { CallbackComponent } from './callback.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindCollabComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
