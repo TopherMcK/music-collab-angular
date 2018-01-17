a14dddedc899e031dd29cb173c4e7efe3a6554b3
TopherMcK <123topher456@gmail.com>
2018-01-17 14:37:18 -0600

    adding find-collab directory

---
+++ src/app/find-collab/find-collab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-collab',
+  templateUrl: './find-collab.component.html',
+  styleUrls: ['./find-collab.component.css']
+})
+export class FindCollabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
