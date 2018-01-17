e4553b4ca1ba43754f06442c129e2e02a8a72064
TopherMcK <123topher456@gmail.com>
2018-01-17 17:00:33 -0600

    adding outline for auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,11 +7,40 @@ import { Router } from '@angular/router';
 @Injectable()
 export class AuthService {
 
-    constructor() { }
+    auth0 = new auth0.WebAuth({
+        clientID: AUTH_CONFIG.CLIENT_ID,
+        domain: AUTH_CONFIG.CLIENT_DOMAIN,
+        responseType: 'token id_token',
+        redirectUri: AUTH_CONFIG.REDIRECT,
+        audience: AUTH_CONFIG.AUDIENCE,
+        scope: AUTH_CONFIG.SCOPE
+    });
+
+    userProfile: any;
+
+    loggedIn: boolean;
+    loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
+
+    constructor(private router: Router) {
+        if (this.authenticated) {
+            this.userProfile = JSON.parse(localStorage.getItem('profile'));
+            this.setLoggedIn(true);
+        } else {
+        }
+    }
+
+    setLoggedIn(value: boolean) {
+        this.loggedIn$.next(value);
+        this.loggedIn = value;
+    }
+
+    login() {
+        this.auth0.authorize();
+    }
 
     get authenticated(): boolean {
         // placeholder while building ui
-        return false;
+        return true;
     }
 
 }
