b9e118371b3a372959f73cfeab3e52e907015f84
TopherMcK <123topher456@gmail.com>
2018-01-17 15:56:16 -0600

    adding html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="container">
-        <router-outlet></router-outlet>
+        <nav class="navbar-header">
+            <div class="navbar-header">
+                <a class="navbar-brand" routerLink="/dashboard"></a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li>
+                    <a routerLink="/Home" routerLinkActive="active">Home</a>
+                </li>
+                <li>
+                    <a routerLink="/Profile" routerLinkActive="active">Profile</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a>Log Out</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="col-sm-12">
+            <router-outlet></router-outlet>
+        </div>
     </div>
     `,
     styles: [
@@ -12,5 +32,6 @@ import { Component } from '@angular/core';
     ]
 })
 export class AppComponent {
-  title = 'Placeholder!!!';
+    title = 'Placeholder!!!';
+    constructor() {}
 }
