17aef4b79c44201550b2bc7e3f5361f1ce6d5e31
Fernando <fernando.m.ism@gmail.com>
2018-01-20 13:17:49 -0600

    Cleaned up CSS and added missing components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,63 @@ import { AuthService } from './auth/auth.service';
         <h1 class="logo">Musicollab</h1>
 
         <span class="welcomeMsg">
-            Hi, welcome to Musicollab! An social community made of musicians looking for others to jam out with. Please begin by logging in or Signing up!
+            Hi, welcome to Musicollab! A social community made of musicians looking for others to jam out with. Please begin by logging in or Signing up!
         </span>
 
         <div class="containerUser">
-            <p class="errorMsgPw">Error Message</p>
+            <p class="errorMsg">Error Message</p>
             Username: <input type="text" name="password" class="password"/>
         </div>
 
         <div class="containerPass">
-            <p class="errorMsgUsr">Error Message</p>
+            <p class="errorMsg">Error Message</p>
             Password: <input type="password" name="username" class="username"/>
         </div>
 
         <input type="button" value="Login" name="Login" class="submitButton">
+
+        
+        <div class="deQueen">
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai"/>
+            <span class="dewai">Home</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai2"/>
+            <span class="dewai2">Contact</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai3"/>
+            <span class="dewai3">About</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai4"/>
+            <span class="dewai4">do u kno de wei??</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai5"/>
+            <span class="dewai5">to hab de wai u must hab  E B O L A</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai6"/>
+            <span class="dewai6">we must conqah</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai7"/>
+            <span class="dewai7">*click**click**click**click**click**click*</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai8"/>
+            <span class="dewai8">*spit**spit**spit**spit**spit**spit*</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai9"/>
+            <span class="dewai9">MAH BRUDA</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai10"/>
+            <span class="dewai10">i will MISS u</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai11"/>
+            <span class="dewai11">false QUEEN</span>
+
+            <img src="https://t00.deviantart.net/uVol_cEfEo8aXY2Bl4je1QIu-50=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/be35/th/pre/f/2018/008/6/b/i_know_de_wey_broddas__render__by_nintega_dario-dbzbzyl.png" class="dewai12"/>
+            <span class="dewai12">u do not kno de wai</span>
+        </div>
+
+        <div class="footer">
+            footer
+        </div>
 
         <nav *ngIf="false" class="navbar navbar-default">
             <div class="default_grid">
@@ -42,7 +85,7 @@ import { AuthService } from './auth/auth.service';
             </ul>
         </nav>
         <div class="col-sm-12">
-<router-outlet></router-outlet>
+            <router-outlet></router-outlet>
         </div>
     </div>
     `,
